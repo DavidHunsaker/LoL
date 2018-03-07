2047b38acf92b98cbba43dc4774d4ccdda813fde
AyrtonStout <AyrtonStout@outlook.com>
2018-03-07 16:26:43 -0700

    Add game window scrolling with the arrow keys

--- GameWindow.java
+++ GameWindow.java
@@ -10,8 +10,16 @@ import java.util.concurrent.TimeUnit;
 
 public class GameWindow extends JPanel {
 
-    private final int INIT_WIDTH = 1600;
-    private final int INIT_HEIGHT = 900;
+    private int viewportWidth = 1600;
+    private int viewportHeight = 900;
+
+    //TODO this shouldn't be defined in the UI code- this should be defined somewhere in the game logic
+    private final int MAP_WIDTH = 2000;
+    private final int MAP_HEIGHT = 1300;
+
+    private int scrollSpeed = 10;
+    private int scrollX = 0;
+    private int scrollY = 0;
 
     private ScheduledExecutorService uiUpdater;
     private Game game;
@@ -20,14 +28,26 @@ public class GameWindow extends JPanel {
         this.game = game;
         setUpUiElements();
 
-        setPreferredSize(new Dimension(INIT_WIDTH, INIT_HEIGHT));
+        setPreferredSize(new Dimension(viewportWidth, viewportHeight));
         this.setDoubleBuffered(true);
         setBackground(Color.black);
 
-        addMouseListener(mouseList);
+        this.addMouseListener(mouseList);
+        this.addKeyListener(keyListener);
         startAnimation();
 
+
+        this.addComponentListener(new ComponentAdapter() {
+            public void componentResized(ComponentEvent e) {
+                super.componentResized(e);
+                viewportWidth = e.getComponent().getWidth();
+                viewportHeight = e.getComponent().getHeight();
+            }
+        });
+
         game.startGame();
+        this.setFocusable(true);
+        this.requestFocus();
     }
 
     public void paintComponent(Graphics g) {
@@ -37,8 +57,12 @@ public class GameWindow extends JPanel {
             g.setColor(entity.getColor());
             // The rounding here is an unfortunate side effect of the fact that everything Swing does seems to be integer based.
             // But a different graphical engine would probably care about the decimal precision
-            g.fillRect((int) Math.round(entity.getX()), (int) Math.round(entity.getY()), entity.getSize(), entity.getSize());
+            g.fillRect((int) Math.round(entity.getX() - scrollX), (int) Math.round(entity.getY() - scrollY), entity.getSize(), entity.getSize());
         });
+
+        // Draw the map boundaries
+        g.setColor(Color.WHITE);
+        g.drawRect(1 - scrollX, 1 - scrollY, MAP_WIDTH - 2, MAP_HEIGHT - 2);
     }
 
     private void setUpUiElements() {
@@ -59,13 +83,53 @@ public class GameWindow extends JPanel {
 
     private MouseListener mouseList = new MouseListener() {
         public void mousePressed(MouseEvent e) {
-            game.getPlayer().moveToPoint(e.getX(), e.getY());
+            game.getPlayer().moveToPoint(e.getX() + scrollX, e.getY() + scrollY);
         }
-
         public void mouseClicked(MouseEvent e) { }
         public void mouseReleased(MouseEvent e) { }
         public void mouseEntered(MouseEvent e) { }
         public void mouseExited(MouseEvent e) { }
+    };
+
+    private KeyListener keyListener = new KeyListener() {
+        public void keyTyped(KeyEvent e) { }
+        public void keyReleased(KeyEvent e) { }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch(e.getKeyCode()) {
+                case 37: // Left
+                    scrollX -= scrollSpeed;
+                    if (scrollX < 0) {
+                        scrollX = 0;
+                    }
+                    break;
+                case 38: // Up
+                    scrollY -= scrollSpeed;
+                    if (scrollY < 0) {
+                        scrollY = 0;
+                    }
+                    break;
+                case 39: // Right
+                    if (viewportWidth > MAP_WIDTH) {
+                        return;
+                    }
+                    scrollX += scrollSpeed;
+                    if (scrollX > MAP_WIDTH - viewportWidth) {
+                        scrollX = MAP_WIDTH - viewportWidth;
+                    }
+                    break;
+                case 40: // Down
+                    if (viewportHeight > MAP_HEIGHT) {
+                        return;
+                    }
+                    scrollY += scrollSpeed;
+                    if (scrollY > MAP_HEIGHT - viewportHeight) {
+                        scrollY = MAP_HEIGHT - viewportHeight;
+                    }
+                    break;
+            }
+        }
     };
 
 }
